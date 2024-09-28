9843166330fd927aa8d459cd6efe0956c5d5359a
Theo Bessel <theo.bessel@outlook.com>
2024-09-28 22:42:34 +0200

    refactor : Move parser code into more appropriate files

--- src/Lang/Parser.hs
+++ src/Lang/Parser.hs
@@ -1,98 +1,80 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Lang.Parser where
-
+{-
+    External imports
+-}
+import Data.Char
 import Control.Applicative
 
--- Define Parser type
-newtype Parser a = P (String -> [(a, String)])
+{-
+    Builtin imports
+-}
+import Parsing.Parser
+import Lang.AST
 
 {-
-    Parsing and evaluation functions
+    Lambda calculus
 -}
-parse :: Parser a -> String -> [(a, String)]
-parse (P p) = p
+identifier :: Parser String
+identifier = check isLower
+    >>= \c -> many (check isAlpha)
+    >>= \s -> return (c:s)
 
-eval :: Show a => Parser a -> String -> a
-eval s p = case parse s p of
-    [(x,[])] -> x
-    [(_,out)] -> error ("UnusedInput :" ++ out)
-    x -> error ("InvalidInput :" ++ show x)
+simpleTerm :: Parser Term
+simpleTerm = func <|> var <|> parens term
+
+term :: Parser Term
+term = app <|> simpleTerm <|> parens simpleTerm
+
+var :: Parser Term
+var = identifier >>= \i -> return $ Var i
+
+func :: Parser Term
+func = string "\\"
+    >> identifier
+    >>= \p -> string ":"
+    >> typeTerm
+    >>= \t -> string "."
+    >> term >>= \b -> return Func {
+        param = p,
+        typ = t,
+        body = b
+    }
+
+app :: Parser Term
+app = simpleTerm
+    >>= \f -> (
+        string " "
+        >> term
+        >>= \a -> return App {
+            fun=f,
+            arg=a
+        }
+    ) <|> return f
 
 {-
-    Parser type
+    Typed lambda calculus
 -}
--- Define Parser type as an instance of Functor
-instance Functor Parser where
-    fmap f p =
-        P ( \s -> case parse p s of
-            [] -> []
-            [(val, out)] -> [(f val, out)]
-            _ -> error "ParsingError"
-        )
+typeIdentifier :: Parser String
+typeIdentifier = check isUpper
+    >>= \c -> many (check isAlpha)
+    >>= \s -> return (c:s)
 
--- Define Parser type as an instance of Applicative
-instance Applicative Parser where
-    pure val = P (\s -> [(val, s)])
-    pf <*> px =
-        P ( \s -> case parse pf s of
-            [] -> []
-            [(fval, out)] -> parse (fmap fval px) out
-            _ -> error "ParsingError"
-        )
+typeVar :: Parser Type
+typeVar = typeIdentifier >>= \i -> return $ TVar i
 
--- Define Parser type as an instance of Monad
-instance Monad Parser where
-    p >>= f =
-        P ( \s -> case parse p s of
-            [] -> []
-            [(val, out)] -> parse (f val) out
-            _ -> error "ParsingError"
-        )
+typeSimpleTerm :: Parser Type
+typeSimpleTerm = typeVar <|> parens typeTerm
 
--- Define Parser type as an instance of Alternative
-instance Alternative Parser where
-    empty = P (const [])
-    many x = some x <|> pure []
-    some x = (:) <$> x <*> many x
-    p1 <|> p2 =
-        P ( \s -> case parse p1 s of
-            [] -> parse p2 s
-            x -> x
-        )
+typeTerm :: Parser Type
+typeTerm = typeFunc <|> typeSimpleTerm
 
-{-
-    Useful parsers
--}
--- Consumes the first element of a string
-item :: Parser Char
-item =
-    P ( \case
-        [] -> []
-        (h : t) -> [(h, t)]
-    )
-
--- Check if the first element satifies a predicate p
-check :: (Char -> Bool) -> Parser Char
-check p = item
-    >>= \x -> if p x
-        then return x
-        else empty
-
--- Check if the elements of a string are equal to a given string
-string :: String -> Parser String
-string [] = return []
-string (h:t) = check (==h)
-    >> string t
-    >> return (h:t)
-
--- Parse one element
-one :: Parser Char -> Parser String
-one p = p >>= \x -> return [x]
-
--- Parse parenthesis
-delimiters :: String -> String -> Parser a -> Parser a
-delimiters l r p = string l >> p >>= \x -> string r >> return x
-
-parens :: Parser a -> Parser a
-parens = delimiters "(" ")"
+typeFunc :: Parser Type
+typeFunc = typeSimpleTerm
+    >>= \p -> (
+        string "->"
+        >> typeTerm
+        >>= \b -> return TFunc {
+            tparam = p,
+            tbody = b
+        }
+    ) <|> return p
